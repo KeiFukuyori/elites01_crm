f2c3f03f1f02ce55ce01b933b4931f2d9df1b64a
k_fukuyori <ocvxkeikei@gmail.com>
2017-01-12 11:01:00 +0000

    Customerモデルに外部キー(post_id)を追加しマイグレーション

--- db/migrate/20170112105520_create_posts.rb
+++ db/migrate/20170112105520_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.string :position_name
+      t.string :position_name, null: false
 
       t.timestamps null: false
     end

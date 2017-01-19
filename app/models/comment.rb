68b72a8bd4b0fa59b49f44bad42f30b2059a48d7
k_fukuyori <ocvxkeikei@gmail.com>
2017-01-19 08:50:13 +0000

    08基本課題2),バリデーションエラーメッセージの日本語化もプラス

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ActiveRecord::Base
   #commentは一人のcustomerに紐づいているので、belongs_toとなる
   belongs_to :customer
   
-  validates :body, presence: true
+  validates :body, 
+    presence: true, length: { maximum: 200 }
   validates :customer_id, presence: true
 end

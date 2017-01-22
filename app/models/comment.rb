68496d107e8d4f56c649e4dc7c5217a5fed8d47b
k_fukuyori <ocvxkeikei@gmail.com>
2017-01-22 08:01:57 +0000

    commentモデルにuser_idカラムを追加。userモデルとcommentsモデルのアソシエーションが完了

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ActiveRecord::Base
   #commentは一人のcustomerに紐づいているので、belongs_toとなる
   belongs_to :customer
+  # commentは一人のuserに紐づいているので、belongs_toとなる
+  belongs_to :user
   
   validates :body, 
     presence: true, length: { maximum: 200 }
